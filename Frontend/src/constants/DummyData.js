17539456089963a497119bedf02cc18106de6290
Sandipan Roy <103448399+Sandipan-Roy-07@users.noreply.github.com>
2025-02-07 09:59:28 +0530

    Create DummyData.js

---
+++ Frontend/src/constants/DummyData.js
@@ -0,0 +1,18 @@
+export const DummyData=[
+    {
+        "strMeal": "Baked salmon with fennel & tomatoes",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/1548772327.jpg",
+    },
+    {
+        "strMeal": "Cajun spiced fish tacos",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/uvuyxu1503067369.jpg",
+    },
+    {
+        "strMeal": "Escovitch Fish",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/1520084413.jpg",
+    },
+    {
+        "strMeal": "Fish fofos",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/a15wsa1614349126.jpg",
+    }
+]
