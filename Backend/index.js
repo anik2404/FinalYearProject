5df12bf4b955d2ff3d2b1c860f888258213837df
Sandipan Roy <103448399+Sandipan-Roy-07@users.noreply.github.com>
2025-02-25 08:36:06 +0530

    Add files via upload

---
+++ Backend/index.js
@@ -0,0 +1,27 @@
+const express=require("express");
+const cors=require("cors");
+const connection = require("./db");
+const userRouter = require("./routes/userRoutes");
+require("dotenv").config();
+const port=process.env.PORT;
+const app=express()
+app.use(cors());
+app.use(express.json());
+app.use("/user",userRouter);
+
+
+app.get("/",async(req,res)=>{
+    res.send("API is working properly")
+})
+
+app.listen(port,async()=>{
+    try{
+        await connection
+        console.log("Database is connected")
+    }
+    catch(err)
+    {
+        console.log(err)
+    }
+    console.log("Server is running on the port number: ",port);
+})
