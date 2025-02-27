777af442123db10134e756097d4c1e637a25e1cf
Sandipan Roy <103448399+Sandipan-Roy-07@users.noreply.github.com>
2025-02-27 08:51:26 +0530

    Add files via upload

--- Frontend/src/navigation/UserStack.js
+++ Frontend/src/navigation/UserStack.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import User from '../screens/User';
 import UserDetails from '../screens/UserDetails';
+import Signup from '../screens/Signup';
+import ForgotPass from '../screens/ForgotPass';
+import ResetPass from '../screens/ResetPass';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +14,9 @@ const UserStack = () => {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="UserMain" component={User} />
       <Stack.Screen name="UserDetails" component={UserDetails} />
+      <Stack.Screen name="Signup" component={Signup} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPass} />
+      <Stack.Screen name="ResetPassword" component={ResetPass} />
     </Stack.Navigator>
   );
 };
