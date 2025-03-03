220e59a2e0ae3b3ec99e0a983506cb6da9555601
Sandipan Roy <103448399+Sandipan-Roy-07@users.noreply.github.com>
2025-03-03 17:19:58 +0530

    Add files via upload

--- Frontend/src/context/AuthContext.js
+++ Frontend/src/context/AuthContext.js
@@ -1,1 +1,13 @@
+import React, { createContext, useState } from "react";
 
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [userid, setuserid] = useState(null);
+
+  return (
+    <AuthContext.Provider value={{ userid, setuserid}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
