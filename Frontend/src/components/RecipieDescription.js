1dbf08f33462992ac6bcb61cd38e44054b072521
Sandipan Roy <103448399+Sandipan-Roy-07@users.noreply.github.com>
2025-03-03 18:44:09 +0530

    Add files via upload

--- Frontend/src/components/RecipieDescription.js
+++ Frontend/src/components/RecipieDescription.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React,{useState,useEffect,useContext} from 'react';
+import { View, Text,TouchableOpacity } from 'react-native';
 import Animated from 'react-native-reanimated';
 import { FadeInDown } from 'react-native-reanimated';
+import { AuthContext } from "../context/AuthContext";
+import Icon from 'react-native-vector-icons/Entypo'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import YoutubeIframe from 'react-native-youtube-iframe';
 
 const RecipieDescription = ({meal}) => {
+    const [isBookmarked, setIsBookmarked] = useState(false);
+    const { userid } = useContext(AuthContext);
+
+    useEffect(() => {
+        checkIfBookmarked();
+    }, []);
+    
+
     const ingredientsIndexes=(meal)=>{
         if(!meal) return [];
         let indexes=[];
@@ -17,6 +27,7 @@ const RecipieDescription = ({meal}) => {
         }
         return indexes;
     }
+
     const getYouTubeVideoId=uri=>{
         const regex= /[?&]v=([^&]+)/;
         const match=uri.match(regex);
@@ -24,20 +35,64 @@ const RecipieDescription = ({meal}) => {
             return match[1];
         return null;
     }
+
+    const toggleBookmark = async () => {
+        if ((userid == null) ||(userid=='null')) {
+            alert('You need to login first to like any recipie')
+        }
+        else{
+            try {
+                const response = await fetch(`http://192.168.104.156:3000/recipie/bookmark`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ idMeal: meal.idMeal, userid })
+                });
+                const data = await response.json();
+                setIsBookmarked(data.bookmarked);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    };  
+    
+    const checkIfBookmarked = async () => {
+        try {
+            const response = await fetch(`http://192.168.104.156:3000/recipie/isBookmarked`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ idMeal: meal.idMeal, userid })
+            });
+            const data = await response.json();
+            setIsBookmarked(data.bookmarked);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    
+
     return (
         <View style={{ flex: 1, paddingTop: hp(4) }}>
-            <Animated.View entering={FadeInDown.duration(700).springify().damping(12)} style={{ marginLeft: wp(3) }}>
-                <Text style={{ fontSize: hp(3), fontWeight: "bold", flex: 1 }}>
-                    {
-                        meal?.strMeal
-                    }
-                </Text>
-                <Text style={{ fontSize: hp(2), fontWeight: "condensedBold", flex: 1, marginTop: hp(0.5),marginBottom:hp(4)}}>
-                    {
-                        meal?.strArea
-                    }
-                </Text>
-            </Animated.View>
+            <View style={{flexDirection:'row'}}>
+                <Animated.View entering={FadeInDown.duration(700).springify().damping(12)} style={{ marginLeft: wp(3) }}>
+                    <Text style={{ fontSize: hp(3), fontWeight: "bold", flex: 1 }}>
+                        {
+                            meal?.strMeal
+                        }
+                    </Text>
+                    <Text style={{ fontSize: hp(2), fontWeight: "condensedBold", flex: 1, marginTop: hp(0.5),marginBottom:hp(4)}}>
+                        {
+                            meal?.strArea
+                        }
+                    </Text>
+                </Animated.View>
+                <Animated.View entering={FadeInDown.duration(700).springify().damping(12)} style={{ marginLeft: wp(3) }}>
+                    <TouchableOpacity onPress={toggleBookmark}
+                    style={{marginLeft:wp(12),backgroundColor:'white',
+                    borderRadius:9999,height:hp(8),width:wp(16),alignItems:'center'}}>
+                        <Icon name="bookmark" size={50} color={isBookmarked ? "orange" : "black"} style={{marginTop:hp(1)}}/>
+                    </TouchableOpacity> 
+                </Animated.View>
+            </View>
             <Animated.View entering={FadeInDown.delay(100).duration(700).springify().damping(12)} style={{marginLeft:wp(3),marginBottom:hp(4),marginRight:wp(3)}}>
                 <Text style={{ fontSize: hp(3), fontWeight: "bold", flex: 1,marginBottom:hp(2)}}>Ingredients</Text>
                 {
