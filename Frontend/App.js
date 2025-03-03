54833e3270b4f274ba2153bce9e8f2c7a36826ef
Sandipan Roy <103448399+Sandipan-Roy-07@users.noreply.github.com>
2025-03-03 17:15:41 +0530

    Add files via upload

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,11 +1,14 @@
 import {NavigationContainer} from '@react-navigation/native';
 import StackNavigation from './src/navigation/StackNavigation';
+import { AuthProvider } from './src/context/AuthContext';
 
 function App() {
   return (
-    <NavigationContainer>
-      <StackNavigation />
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <StackNavigation />
+      </NavigationContainer>
+    </AuthProvider>
   );
 }
 
