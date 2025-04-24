8e251136fdd20008063926a6f5a7716e5dcaac85
Sandipan Roy <103448399+Sandipan-Roy-07@users.noreply.github.com>
2025-04-24 08:46:08 +0530

    Add files via upload

---
+++ Backend/models/AddRecipieModel.js
@@ -0,0 +1,15 @@
+const mongoose=require("mongoose");
+
+const addRecipieSchema=mongoose.Schema({
+    rname:{type:String,required:true},
+    region:{type:String,required:true},
+    instructions:{type:String,required:true},
+    avatar:{type:String,required:true},
+    //userid:{type:String,required:true},
+},{
+    versionKey:false
+})
+
+const addRecipieModel=mongoose.model("addedrecipies",addRecipieSchema);
+
+module.exports=addRecipieModel
